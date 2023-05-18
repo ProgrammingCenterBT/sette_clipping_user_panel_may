5ae692303388ca939110f3b063f0e5cf13f12a3c
dsoftver <dsoftver@gmail.com>
2023-05-18 23:56:01 +0200

    install express

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://clipping-user-panel-may.herokuapp.com/'
+  apiBaseUrl: 'https://clipping-java-backend-may-2023.herokuapp.com/'
 };
